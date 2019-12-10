d15610b66f076aa90cbac5eef61c8548da88557f
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-09 18:11:18 -0600

    primeros pasos con el formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Fragment} from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
-      <Header 
-        titulo='Clima React App'
-      />
+        <Header 
+          titulo='Clima React App'
+        />
+
+        <div className="contenedor-form">
+            <div className="container">
+                <div className="row">
+                    <div className="col m6 s12">
+                        <Formulario />
+                    </div>
+                    <div className="col m6 s12">
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
     </Fragment>
   );
 }

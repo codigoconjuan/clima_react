459ea6e2864479de84a388fa0b13855bef3fdc89
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-09 19:03:05 -0600

    mostrando el resultado de la consulta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       pais: ''
   });
   const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
 
   const { ciudad, pais } = busqueda;
 
@@ -23,7 +25,8 @@ function App() {
           const respuesta = await fetch(url);
           const resultado = await respuesta.json();
   
-          console.log(resultado);
+          guardarResultado(resultado);
+          guardarConsultar(false);
         }
         
     }
@@ -48,7 +51,9 @@ function App() {
                         />
                     </div>
                     <div className="col m6 s12">
-                        2
+                        <Clima 
+                          resultado={resultado}
+                        />
                     </div>
                 </div>
             </div>
